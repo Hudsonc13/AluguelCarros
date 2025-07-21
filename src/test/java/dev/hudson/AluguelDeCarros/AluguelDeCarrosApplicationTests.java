2490bdc1c4e96045b7ae0219236b4d491abd7a13
Hudson Clementino <122533612+Hudsonc13@users.noreply.github.com>
2025-07-21 19:42:03 -0300

    Inicio do Projeto
    Codigo gerado pelo Spring Initializr

---
+++ src/test/java/dev/hudson/AluguelDeCarros/AluguelDeCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hudson.AluguelDeCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelDeCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
